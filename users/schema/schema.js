b0d9802458b0daaecef9b5c5d31c86e22b3fff2f
Joey Feng <jfeng702@gmail.com>
2019-09-30 21:23:03 -0400

    update

---
+++ users/schema/schema.js
@@ -0,0 +1,27 @@
+const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const users = [
+  { id: "23", firstName: "Bill", age: 20 },
+  { id: "47", firstName: "Samantha", age: 21 },
+  {}
+];
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    type: UserType,
+    args: { id: { type: GraphQLString } },
+    resolve(parentValue, args) {}
+  }
+});
